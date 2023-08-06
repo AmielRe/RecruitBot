57103c8bd1c35d470d682547ed46974e22d74aaf
AmielRe <amieli1999@gmail.com>
2023-08-06 21:47:22 +0300

    Export error message to file.
    All error messages are in single file.
    Organize and simplify the code.

---
+++ error_messages.py
@@ -0,0 +1,6 @@
+INVALID_CHAT_DATA = "Invalid chat data"
+ERROR_FETCHING_CHAT_DATA = "Error fetching chat data"
+ERROR_FETCHING_QUESTION_DATA = "Error fetching question data"
+ERROR_FETCHING_ANSWER_DATA = "Error fetching answer data"
+QUERY_FILE_NOT_FOUND = "Query file not found"
+REQUEST_PROCESSING_ERROR = "An error occurred while processing the request"
